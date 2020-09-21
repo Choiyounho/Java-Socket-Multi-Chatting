baa2df5767820d83bcba5e41a05abddcfb6f396c
Choiyounho <maxosa78@gmail.com>
2020-09-22 03:36:07 +0900

    [feat] : Socket 서버와 클라이언트 구현

---
+++ src/main/java/Client.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("127.0.0.1", 9999);
+            System.out.println("Connection Success!");
+
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.next();
+            OutputStream outputStream = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outputStream); // 한글이 깨지는 것을 방지
+            dataOutputStream.writeUTF(message);
+
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            System.out.println("Receive : " + dataInputStream.readUTF());
+
+            dataInputStream.close();
+            dataOutputStream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("IOException e " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception e " + e.getMessage());
+        }
+    }
+}
+
---
+++ src/main/java/Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(9999);
+            System.out.println("Server ready ...");
+
+        } catch (IOException e) {
+            System.out.println("IOException e " + e.getMessage());
+        }
+
+        while (true) {
+            try {
+                try {
+                    Socket socket = serverSocket.accept(); // 클라이언트 정보를 알고 있는 소켓
+                    System.out.println("client connect success!");
+
+                    InputStream inputStream = socket.getInputStream();
+                    DataInputStream dataInputStream = new DataInputStream(inputStream);
+                    String message = dataInputStream.readUTF();
+
+                    OutputStream outputStream = socket.getOutputStream();
+                    DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+                    dataOutputStream.writeUTF("[ECHO]" + message + "(from Server!)");
+
+                    dataOutputStream.close();
+                    dataInputStream.close();
+                    socket.close();
+                    System.out.println("client Socket close...");
+
+                } catch (IOException e) {
+                    System.out.println("IOException e " + e.getMessage());
+                }
+            } catch (Exception e) {
+                System.out.println("Exception e " + e.getMessage());
+
+            }
+        }
+
+    }
+}
