8a4dfce69f2647bd854af8b191df678af78778b0
Choiyounho <maxosa78@gmail.com>
2020-10-08 15:19:08 +0900

    [feat] : IOApi 클래스 구현

---
+++ src/main/java/chat/domain/IOApi.java
@@ -0,0 +1,24 @@
+package chat.domain;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class IOApi {
+
+    public static DataOutputStream newInstance4DataOutputStream(Socket socket) throws IOException {
+        return new DataOutputStream(socket.getOutputStream());
+    }
+
+    public static DataInputStream newInstance4DataInputStream(Socket socket) throws IOException {
+        return new DataInputStream(socket.getInputStream());
+    }
+
+    public static void closeIOStream(Socket socket, DataInputStream dataInputStream, DataOutputStream dataOutputStream) throws IOException {
+        dataOutputStream.close();
+        dataInputStream.close();
+        socket.close();
+    }
+
+}
